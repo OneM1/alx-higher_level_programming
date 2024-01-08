f83974de7748489486f7fb2c4d3874c35f27510d
OneM1 <mouadelhourre50@gmail.com>
2024-01-08 14:40:11 +0300

    multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        sentence = None
+    if sentence:
+        length = len(sentence)
+    else:
+        length = 0
+    return (length, sentence if not sentence else sentence[:1])
+
