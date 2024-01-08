11804b61b16f2119073d139db75050f455c972ab
OneM1 <mouadelhourre50@gmail.com>
2024-01-08 03:23:16 +0300

    reverse order

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-def print_reversed_list_integer(my_list=[]):
-    reverse = my_list[::-1]
+def print_reversed_list_integer(my_list=None):
+    if my_list is None:
+        my_list=[]
+    reverse=my_list[::-1]
     for i in reverse:
         print("{}".format(i))
