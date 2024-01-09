ea77a817feb7dad153798c821aa1f74a1c20f92d
OneM1 <mouadelhourre50@gmail.com>
2024-01-09 10:14:18 +0300

    last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,14 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 number_str = str(number)
-last_digit = number_str[-1:]
+Last_digit = number_str[-1:]
 text = "Last digit of"
 text2 = "and is less than 6 and not 0"
-if int(last_digit) > 5:
-    print("{} {} is {} and is great than 5".format(text, number, last_digit))
-elif int(last_digit == 0):
-    print("{} {} is {} and is 0".format(text, number, last_digit))
-elif int(last_digit) < 6 and last_digit != '0':
-    print("{} {} is {} {}".format(text, number, last_digit, text2))
+if int(Last_digit) > 5:
+    print("{} {} is {} and is great than 5".format(text, number, Last_digit))
+elif int(Last_digit) == 0:
+    print("{} {} is {} and is 0".format(text, number, Last_digit))
+elif int(Last_digit) < 6 and Last_digit != '0':
+    print("{} {} is {} {}".format(text, number, Last_digit, text2))
 else:
     print("Wrong Type")
