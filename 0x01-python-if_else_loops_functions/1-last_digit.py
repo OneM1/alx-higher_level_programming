d10670ae0786f7775a690533945d737ca3506894
OneM1 <mouadelhourre50@gmail.com>
2024-01-09 06:51:18 +0300

    last difit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,10 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 number_str = str(number)
 last_digit = number_str[-1:]
-
+text = "Last digit of"
+text2 = "and is less than 6 and not 0"
 if int(last_digit) > 5:
-    print("Last digit of {} is {} and is great than 5".format(number, last_digit))
+    print("{} {} is {} and is great than 5".format(text, number, last_digit))
 elif int(last_digit == 0):
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("{} {} is {} and is 0".format(text, number, last_digit))
 elif int(last_digit) < 6 and last_digit != '0':
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print("{} {} is {} {}".format(text, number, last_digit, text2))
