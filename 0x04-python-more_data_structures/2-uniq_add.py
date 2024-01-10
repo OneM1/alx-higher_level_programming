fb8213b93dd5bf243289006a59da867db81c3227
OneM1 <mouadelhourre50@gmail.com>
2024-01-10 13:35:12 +0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_int = set()
-    return sum(map(lambda x: unique_int.add(x) or x if x not in unique_int else 0, my_list))
+    return sum(
+            map(
+                lambda x: unique_int.add(x) or x if x not in unique_int else 0,
+                my_list
+                )
+            )
