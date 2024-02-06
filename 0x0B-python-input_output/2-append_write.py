3f3b2873ae953439856e25369769ac761cc3cce7
OneM1 <mouadelhourre50@gmail.com>
2024-02-06 05:09:19 -0800

    0x0B-python-input_output

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Module to append a string at end  return number of characters added.
+"""
+
+
+def append_write(filename="", text=""):
+    """Append a string at the end of file return number of characters added.
+
+    Args:
+        filename (str): The name of the text file to append to.
+        text (str): The string to append to the file.
+
+    Returns:
+        int: The number of characters added.
+    """
+    with open(filename, mode='a', encoding='utf-8') as file:
+        return file.write(text)
+
+
+if __name__ == "__main__":
+    print(append_write())
+
