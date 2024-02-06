e079c3d58b55643b0b1086fee4739de96f4d7a03
OneM1 <mouadelhourre50@gmail.com>
2024-02-06 05:00:14 -0800

    string to text

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to write a string to a text file and return the number of characters written."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file and return the number of characters written.
+
+    Args:
+        filename (str): The name of the text file to write to.
+        text (str): The string to write to the file.
+
+    Returns:
+        int: The number of characters written.
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
+
+
+if __name__ == "__main__":
+    print(write_file())
