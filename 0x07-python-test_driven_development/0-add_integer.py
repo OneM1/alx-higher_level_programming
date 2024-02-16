3bd278fa646a9719b125082ba76b50455ffc75ff
OneM1 <mouadelhourre50@gmail.com>
2024-02-16 16:31:47 +0800

    add integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,9 @@ def add_integer(a, b=98):
     Add 2 integers.
 
     Prototype: def add_integer(a, b=98):
-    a and b must be integers or floats, otherwise raise a TypeError exception with the message a must be an integer or b must be an integer
+    a and b must be integers or floats, otherwise raise a
+    TypeError exception with the message a must be an integer
+    or b must be an integer
     a and b must be first casted to integers if they are float
     Returns an integer: the addition of a and b
     You are not allowed to import any module
@@ -18,6 +20,8 @@ def add_integer(a, b=98):
     b = int(b)
 
     return a + b
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
